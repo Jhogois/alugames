a9119a9acc3cc69c203b2592c4c7e176e7a2aa14
Jhonathan Gois <jhogois@live.com>
2024-08-28 20:37:26 -0300

    Novo desafio feito

---
+++ js/app.js
@@ -0,0 +1,25 @@
+function fraseBotao(botao, texto) {
+  let button = document.getElementById(`game-${botao}`).querySelector("a");
+
+  button.innerHTML = texto;
+}
+
+fraseBotao(1, "Alugar");
+fraseBotao(2, "Alugar");
+fraseBotao(3, "Devolver");
+
+function alterarStatus(jogo) {
+  let selecionaJogo = document.getElementById(`game-${jogo}`);
+  let game = selecionaJogo.querySelector("div");
+  let button = selecionaJogo.querySelector("a");
+
+  if (!!button.innerHTML === "Alugar") {
+    game.classList.add("dashboard__item__img--rented");
+    button.classList.add("dashboard__item__button--return");
+    fraseBotao(jogo, "Devolver");
+  } else if (button.innerHTML === "Devolver") {
+    game.classList.remove("dashboard__item__img--rented");
+    button.classList.remove("dashboard__item__button--return");
+    fraseBotao(jogo, "Alugar");
+  }
+}
